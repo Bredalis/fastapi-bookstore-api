018cb251461cc62eb501f480fdb9a6c7ae5c2370
PRO X <bredalisgautreaux@gmail.com>
2023-12-11 10:39:12 -0400

    models.py (#1)
    Some comments

--- models.py
+++ models.py
@@ -1,7 +1,9 @@
 from pydantic import BaseModel
 
+# Define a Book class inheriting from BaseModel
 class Book(BaseModel):
-    title: str
-    author: str
-    isbn: str
-    publication_year: int
+    # Fields representing book attributes with their respective data types
+    title: str  # Title of the book (string)
+    author: str  # Author of the book (string)
+    isbn: str  # ISBN (International Standard Book Number) of the book (string)
+    publication_year: int  # Year of publication (integer)
